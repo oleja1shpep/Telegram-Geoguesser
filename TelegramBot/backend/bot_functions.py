6f6098d92dae1d4fef7efb389005460c0684f32a
Ilya Govzman <govzman@list.ru>
2024-04-25 20:24:29 +0300

    add Google Static Map

--- TelegramBot/backend/bot_functions.py
+++ TelegramBot/backend/bot_functions.py
@@ -48,7 +48,9 @@ async def gpt_request(cords, language):
 
 async def get_url(cords):
     lat1, lon1, lat2, lon2 = map(float, cords.split())
-    return f"https://static-maps.yandex.ru/v1?pl=c:8822DDC0,w:3,{lon1},{lat1},{lon2},{lat2}&pt={lon1},{lat1},flag~{lon2},{lat2},comma&apikey={TOKEN_STATIC}"
+    return f"https://maps.googleapis.com/maps/api/staticmap?path=color:0x0000ff80|weight:5|{lon1},{lat1}|\
+       {lon2},{lat2}&markers=icon:https://storage.yandexcloud.net/test-geoguessr/correct_marker.png|{lon1},{lat1}&markers=icon:https://\
+            storage.yandexcloud.net/test-geoguessr/marker.png|{lon2},{lat2}|&size=400x400&key=AIzaSyB90M6YMN89duMBupapc6x7_K8gRNGw7sk"
 
 async def calculate_score_and_distance(cords):
     lat1, lon1, lat2, lon2 = map(float, cords.split())
