c4ef7926638a7a1f99a7b782ddf6719806ede386
Говзман Илья Денисович <idgovzman_2@edu.hse.ru>
2024-05-14 02:59:18 +0300

    Add map ids for light and dark schemes

--- MiniApp/app.js
+++ MiniApp/app.js
@@ -65,97 +65,16 @@ async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-    styles = []
-    console.log(window.Telegram.WebApp.colorScheme)
-    // if (window.Telegram.WebApp.colorScheme == "dark") {
-    //     styles = [
-        //     { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-        //     { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-        //     { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-        //     {
-        //       featureType: "administrative.locality",
-        //       elementType: "labels.text.fill",
-        //       stylers: [{ color: "#d59563" }],
-        //     },
-        //     {
-        //       featureType: "poi",
-        //       elementType: "labels.text.fill",
-        //       stylers: [{ color: "#d59563" }],
-        //     },
-        //     {
-        //       featureType: "poi.park",
-        //       elementType: "geometry",
-        //       stylers: [{ color: "#263c3f" }],
-        //     },
-        //     {
-        //       featureType: "poi.park",
-        //       elementType: "labels.text.fill",
-        //       stylers: [{ color: "#6b9a76" }],
-        //     },
-        //     {
-        //       featureType: "road",
-        //       elementType: "geometry",
-        //       stylers: [{ color: "#38414e" }],
-        //     },
-        //     {
-        //       featureType: "road",
-        //       elementType: "geometry.stroke",
-        //       stylers: [{ color: "#212a37" }],
-        //     },
-        //     {
-        //       featureType: "road",
-        //       elementType: "labels.text.fill",
-        //       stylers: [{ color: "#9ca5b3" }],
-        //     },
-        //     {
-        //       featureType: "road.highway",
-        //       elementType: "geometry",
-        //       stylers: [{ color: "#746855" }],
-        //     },
-        //     {
-        //       featureType: "road.highway",
-        //       elementType: "geometry.stroke",
-        //       stylers: [{ color: "#1f2835" }],
-        //     },
-        //     {
-        //       featureType: "road.highway",
-        //       elementType: "labels.text.fill",
-        //       stylers: [{ color: "#f3d19c" }],
-        //     },
-        //     {
-        //       featureType: "transit",
-        //       elementType: "geometry",
-        //       stylers: [{ color: "#2f3948" }],
-        //     },
-        //     {
-        //       featureType: "transit.station",
-        //       elementType: "labels.text.fill",
-        //       stylers: [{ color: "#d59563" }],
-        //     },
-        //     {
-        //       featureType: "water",
-        //       elementType: "geometry",
-        //       stylers: [{ color: "#17263c" }],
-        //     },
-        //     {
-        //       featureType: "water",
-        //       elementType: "labels.text.fill",
-        //       stylers: [{ color: "#515c6d" }],
-        //     },
-        //     {
-        //       featureType: "water",
-        //       elementType: "labels.text.stroke",
-        //       stylers: [{ color: "#17263c" }],
-        //     },
-        //   ]
-    // }
+    mapId = "799bb53730cb6698"
+    if (window.Telegram.WebApp.colorScheme == "dark") {
+        mapId = "96181ab43c554867";
+    }
 
     map = new Map(document.getElementById("map"), {
         controls: {},
         clickableIcons: false,
         disableDefaultUI: true,
         zoom: zoom,
-        // styles: styles,
         minZoom: 1, 
         center: {lat: x_center, lng: y_center},
         restriction: {
@@ -166,7 +85,7 @@ async function initMap() {
               west: -180,
             },
         },
-        mapId: "799bb53730cb6698"
+        mapId: mapId
     });
 
     const markerImg = document.createElement("img");
